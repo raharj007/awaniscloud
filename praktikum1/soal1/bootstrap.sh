ca1f16f91d21574651af4690144daf24101c1193
cahyasetya <cahya.setyaa@gmail.com>
2017-03-11 21:55:46 +0700

    memperbaiki bootstrap.sh untuk install php7.0 & apache2 dengan repository ondrej. dan menambahkan synchronized folder untuk tes apakah php7.0 berfungsi atau tidak

--- praktikum1/soal1/bootstrap.sh
+++ praktikum1/soal1/bootstrap.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-apt-get install -y python-software-properties
-add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y apache2
-apt-get install -y php7.0
+apt-get install -y python-software-properties && add-apt-repository ppa:ondrej/apache2 && add-apt-repository ppa:ondrej/php && apt-get update && apt-get install -y apache2 php7.0 libapache2-mod-php7.0 && apt-get install -y
