2dbee8cb4db1d4d97d77fe7f30160d23a568441f
cahyasetya <cahya.setyaa@gmail.com>
2017-03-12 15:43:24 +0700

    memperbaiki bootstrap

--- praktikum1/soal1/bootstrap.sh
+++ praktikum1/soal1/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y python-software-properties && add-apt-repository ppa:ondrej/apache2 && add-apt-repository ppa:ondrej/php && apt-get update && apt-get install -y apache2 php7.0 libapache2-mod-php7.0 && apt-get install -y
+apt-get install -y python-software-properties && add-apt-repository ppa:ondrej/apache2 && add-apt-repository ppa:ondrej/php && apt-get update && apt-get install -y apache2 php7.0 libapache2-mod-php7.0 && apt-get install -y || echo "instalasi gagal"
