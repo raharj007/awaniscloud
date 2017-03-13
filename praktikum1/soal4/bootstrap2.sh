9eea01b0c525c7fcc7b7767581e2cb37a122da3e
cahyasetya <cahya.setyaa@gmail.com>
2017-03-13 16:29:22 +0700

    menghilangkan akses root dan ganti metode instalasi ruby dari rbenv ke rvm

---
+++ praktikum1/soal4/bootstrap2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+printf "support library installed\n\n\n\n\n";
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.0
+rvm use 2.4.0 --default
+ruby -v
+gem install bundler
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+gem install rails -v 5.0.1
+rails -v
